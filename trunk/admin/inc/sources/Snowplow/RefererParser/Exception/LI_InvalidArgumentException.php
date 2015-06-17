1b011a633612f185da4f8f3bb81e9c1a097dbbdf
AndyGCook <AndyGCook@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2015-06-17 21:08:03 +0000

    deleting double trunk from 3.1.9 tag
    git-svn-id: https://plugins.svn.wordpress.org/leadin/tags/3.1.9@1182902 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- trunk/admin/inc/sources/Snowplow/RefererParser/Exception/LI_InvalidArgumentException.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-class LI_InvalidArgumentException extends BaseInvalidArgumentException
-{
-    public static function fileNotExists($fileName)
-    {
-        return new static(sprintf('File "%s" does not exist', $fileName));
-    }
-}
